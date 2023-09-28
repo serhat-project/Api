377bfbde4737cc7b1d2f964b90420dcaf1704c8f
serhat-project <sucar661@gmail.com>
2023-09-29 00:54:00 +0300

    extent last added

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -7,7 +7,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = {"src/test/resources/features"},
         glue = {"stepdefs"},
-        tags = "@api2",
+        tags = "@api1",
         plugin = { "pretty",
                 "json:test-output/cucumber-reports/cucumber.json",
                 "html:test-output/cucumber-reports/cucumberreport.html",
